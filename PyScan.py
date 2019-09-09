5256c1d18841620b9c86c0ee900df53930ae9a9c
Lisandro Luna <lisandro_luna@ymail.com>
2019-09-09 16:25:02 -0300

    Se agregan nuevas funciones.

--- PyScan.py
+++ PyScan.py
@@ -2,18 +2,25 @@ import netifaces
 import subprocess as sp
 import nmap
 import socket
+import argparse
 
-def getBitsNetmask(netmask):
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--iface", help="Ingresar interfaz a utilizar", action="store_true")
+args = parser.parse_args()
+
+
+def getbitsnetmask(netmask):
     return sum([bin(int(x)).count('1') for x in netmask.split('.')])
 
 
-def getHostNum(bits):
+def gethostnum(bits):
     mbits = 32 - bits
-    nhost = 2**mbits
+    nhost = 2 ** mbits
     return nhost
 
 
-def splitIp(ip):
+def splitip(ip):
     ipdata = ip.split('.')
     ipdata[0] = int(ipdata[0])
     ipdata[1] = int(ipdata[1])
@@ -22,9 +29,9 @@ def splitIp(ip):
     return ipdata
 
 
-def getStarterIp(broad, nhosts):
-    ipdata = splitIp(broad)
-    for i in range(0, nhosts-1):
+def getstarterip(broad, nhosts):
+    ipdata = splitip(broad)
+    for i in range(0, nhosts - 1):
         if ipdata[3] > 0:
             ipdata[3] = ipdata[3] - 1
         else:
@@ -42,17 +49,17 @@ def getStarterIp(broad, nhosts):
     return datastr
 
 
-def scanIp(start, nhosts):
-    startip = splitIp(start)
+def scanip(start, nhosts):
+    startip = splitip(start)
     datastr = str(startip[0]) + '.' + str(startip[1]) + '.' + str(startip[2]) + '.' + str(startip[3])
-    #Limito la cantidad de host a escanear a 10 para evitar demoras
-    #for i in range(0, nhosts-1):
+    # Limito la cantidad de host a escanear a 10 para evitar demoras
+    # for i in range(0, nhosts-1):
     for i in range(0, 11):
         print('\nIP a analizar: ' + datastr)
-        if getPing(datastr) == True:
-            analizeIp(datastr)
+        if getping(datastr) == True:
+            analizeip(datastr)
         else:
-            print('    - Sin respuesta.')
+            print('    - Sin respuesta a ping.')
         if startip[3] <= 255:
             startip[3] = startip[3] + 1
         else:
@@ -69,53 +76,58 @@ def scanIp(start, nhosts):
         datastr = str(startip[0]) + '.' + str(startip[1]) + '.' + str(startip[2]) + '.' + str(startip[3])
 
 
-def analizeIp(host):
+def analizeip(host):
     try:
         nm = nmap.PortScanner()
         try:
-            r = nm.scan(host, arguments='-sT -w 5')
+            r = nm.scan(host, arguments='-sT -T5')
             print('    - Puertos TCP:')
             getportsinfo(nm[host]['tcp'], host)
         except Exception as e:
             print('        - Error al leer puertos TCP.')
         try:
-            r = nm.scan(host, arguments='-sU -w 5')
+            r = nm.scan(host, arguments='-sU -T5')
             print('    - Puertos UDP:')
             getportsinfo(nm[host]['udp'], host)
         except Exception as e:
             print('        -  Error al leer puertos UDP.')
     except Exception as e:
-            print('    - Error al Escanear.')
+        print('    - Error al Escanear.')
 
 
 def getbannerdata(ip, port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((ip, port))
-        banner = sock.recv(1024)
+        s = socket.socket()
+        s.connect((ip, port))
+        banner = s.recv(1024)
+        banner = str(banner).replace('b\'', '').replace('\'', '').rstrip('\r\n')
+        banner = (banner[:75] + '..') if len(banner) > 75 else banner
         return banner
     except Exception as e:
-        return 'N/A'
+        return str(e)
 
 
 def getportsinfo(data, host):
     for k in data:
-        banner = getbannerdata(host, data)
-        print('        - ' + str(k) + ': ' + banner)
+        try:
+            banner = getbannerdata(host, int(k))
+            print('        - ' + str(k) + ': ' + banner)
+        except Exception as e:
+            return 'N/A' + str(e)
 
 
-def getListIf(ifli):
-    for i in (0, (len(ifli)-1)):
+def getlistif(ifli):
+    for i in (0, (len(ifli) - 1)):
         print('    - ' + ifli[i])
 
 
-def getIfData(iface):
-        data = netifaces.ifaddresses(iface)
-        ipv4data = data[netifaces.AF_INET]
-        return ipv4data
+def getifdata(iface):
+    data = netifaces.ifaddresses(iface)
+    ipv4data = data[netifaces.AF_INET]
+    return ipv4data
 
 
-def getPing(ip):
+def getping(ip):
     try:
         status, result = sp.getstatusoutput("ping -c1 -w2 " + ip)
 
@@ -127,7 +139,7 @@ def getPing(ip):
         return False
 
 
-def checkKey(dict, key):
+def checkkey(dict, key):
     if key in dict.keys():
         return True
     else:
@@ -135,22 +147,28 @@ def checkKey(dict, key):
 
 
 print('Bienvenido PyScan')
-print('\nInterfaces disponibles: ')
-getListIf(netifaces.interfaces())
-ifa = str(input('\nIngrese la interfaz a utilizar: '))
+iface =  args.iface
 print('')
-ifdata = getIfData(ifa)
-nbits = getBitsNetmask(ifdata[0]['netmask'])
-hosts = getHostNum(nbits)
-shost = getStarterIp(ifdata[0]['broadcast'], hosts)
-print('    - IP: ' + ifdata[0]['addr'])
-print('    - Netmask: ' + ifdata[0]['netmask'])
-print('    - Broadcast: ' + ifdata[0]['broadcast'])
-print('    - Hosts: ' + str(hosts))
-print('    - Tipo de red: ' + str(shost) + '/' + str(nbits))
-print('\nComenzando analisis de red, solo se analizaran los host que respondan ping.')
-print('-Escaneo limitado a 10 hosts-')
-print('Iniciando...')
-#scanIp('10.40.7.20', hosts)
-scanIp(shost, hosts)
-
+if iface == 'lo':
+    print('El script no escanea redes lcoales!')
+elif iface == 'localhost':
+    print('El script no escanea redes lcoales!')
+else:
+    ifaces = getlistif(netifaces.interfaces())
+    if iface in ifaces:
+        ifdata = getifdata(iface)
+        nbits = getbitsnetmask(ifdata[0]['netmask'])
+        hosts = gethostnum(nbits)
+        shost = getstarterip(ifdata[0]['broadcast'], hosts)
+        print('    - IP: ' + ifdata[0]['addr'])
+        print('    - Netmask: ' + ifdata[0]['netmask'])
+        print('    - Broadcast: ' + ifdata[0]['broadcast'])
+        print('    - Hosts: ' + str(hosts))
+        print('    - Tipo de red: ' + str(shost) + '/' + str(nbits))
+        print('\nComenzando analisis de red, solo se analizaran los host que respondan ping.')
+        print('-Escaneo limitado a 10 hosts-')
+        print('Iniciando...')
+        # scanIp('10.40.7.20', hosts)
+        scanip(shost, hosts)
+    else:
+        print('Interfaz no valida!')
